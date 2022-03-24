aca2c97ae51a1761ac4988d5959d3e9c48eb8026
Lee McCormick <lmccormick@corp.m3ts.com>
2022-03-24 12:05:46 -0500

    Chapter 5 Accessing Objects From Code

--- WiredBrainCoffee/MainPage.xaml.cs
+++ WiredBrainCoffee/MainPage.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Popups;
@@ -13,30 +15,66 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using WiredBrainCoffee.DataProvider;
+using WiredBrainCoffee.Model;
 
 namespace WiredBrainCoffee
 {
     public sealed partial class MainPage : Page
     {
+        private CustomerDataProvider _customerDataProvider;
+
         public MainPage()
         {
             this.InitializeComponent();
+            this.Loaded += MainPage_Loaded;
+            App.Current.Suspending += App_Suspending;
+            _customerDataProvider = new CustomerDataProvider();
         }
+
+   
+        private async void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            // throw new NotImplementedException();
+            customerListView.Items.Clear();
+            var customers = await _customerDataProvider.LoadCustomersAsync();
+            foreach (var customer in customers)
+            {
+                customerListView.Items.Add(customer);
+            }
+        }
+
+        private async void App_Suspending(object sender, SuspendingEventArgs e)
+        {
+            // throw new NotImplementedException();
+            var deferral = e.SuspendingOperation.GetDeferral();
+            await _customerDataProvider.SaveCustomersAsync(
+                customerListView.Items.OfType<Customer>());
+            deferral.Complete(); // To close application when async done
+        }
+
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
-        private async void ButtonAddCustomer_Click(object sender, RoutedEventArgs e)
+        private void ButtonAddCustomer_Click(object sender, RoutedEventArgs e)
         {
-            var messageDialog = new MessageDialog("Customer added");
-            await messageDialog.ShowAsync();
+            //var messageDialog = new MessageDialog("Customer added");
+            //await messageDialog.ShowAsync();
+            var customer = new Customer { FirstName = "New" };
+            customerListView.Items.Add(customer);
+            customerListView.SelectedItem = customer;
         }
 
         private void ButtonDeleteCustomer_Click(object sender, RoutedEventArgs e)
         {
-
+            var customer = customerListView.SelectedItem as Customer;
+            if (customer != null)
+            {
+                customerListView.Items.Remove(customer);
+            }
         }
 
         private void ButtonMove_Click(object sender, RoutedEventArgs e)
@@ -47,6 +85,35 @@ namespace WiredBrainCoffee
             // customerListGrid.SetValue(Grid.ColumnProperty, newColumn);
             Grid.SetColumn(customerListGrid, newColumn);
             moveSymbol.Symbol = newColumn == 0 ? Symbol.Forward : Symbol.Back;
+        }
+
+        private void CustomerListView_SeletionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var customer = customerListView.SelectedItem as Customer;
+            txtFirstName.Text = customer?.FirstName ?? "";
+            txtLastName.Text = customer?.LastName ?? "";
+            chkIsDeveloper.IsChecked = customer?.IsDeveloper;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateCustomer();
+        }
+
+        private void CheckBox_IsCheckedChanged(object sender, RoutedEventArgs e)
+        {
+            UpdateCustomer();
+        }
+
+        private void UpdateCustomer()
+        {
+            var customer = customerListView.SelectedItem as Customer;
+            if (customer != null)
+            {
+                customer.FirstName = txtFirstName.Text;
+                customer.LastName = txtLastName.Text;
+                customer.IsDeveloper = chkIsDeveloper.IsChecked.GetValueOrDefault();
+            }
         }
     }
 }
---
+++ WiredBrainCoffee/Model/Customer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using WiredBrainCoffee.Base;
+
+namespace WiredBrainCoffee.Model
+{
+    public class Customer : Observable
+    {
+        private string _firstName;
+        private string _lastName;
+        private bool isDeveloper;
+
+        public string FirstName
+        {
+            get => _firstName;
+            set
+            {
+                _firstName = value;
+                OnPropertyChanged();
+            }
+        }
+        public string LastName
+        {
+            get => _lastName;
+            set
+            {
+                _lastName = value;
+                OnPropertyChanged();
+            }
+        }
+        public bool IsDeveloper
+        {
+            get => isDeveloper;
+            set
+            {
+                isDeveloper = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
+
+
